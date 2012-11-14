8d785a397757aa936745ca73862fa9811037134e
Amaury Denoyelle <amaury.denoyelle@gmail.com>
2012-11-14 10:34:18 +0100

    Adjust ball painting
    Adjust ball painting functions like Ball::boudingRect() and
    Ball::paint()

--- Ball.cpp
+++ Ball.cpp
@@ -4,7 +4,7 @@
 #include <QPainter>
 
 #define CIRCLE_RADIUS 15
-#define SPEED 1
+#define SPEED 3
 #define PI 3.14159265
 
 Ball::Ball()
@@ -14,8 +14,9 @@ Ball::Ball()
 
 QRectF Ball::boundingRect() const
 {
-    return QRectF(-CIRCLE_RADIUS, -CIRCLE_RADIUS, 
-                    CIRCLE_RADIUS, CIRCLE_RADIUS);
+    qreal adjust = 0.5;
+    return QRectF(-adjust -CIRCLE_RADIUS, -adjust -CIRCLE_RADIUS, 
+                    adjust + CIRCLE_RADIUS, adjust + CIRCLE_RADIUS);
 }
 
 void Ball::paint(QPainter * painter, const QStyleOptionGraphicsItem * options, QWidget * widget)
@@ -30,11 +31,11 @@ void Ball::advance(int phase)
         return;
 
     QPointF scene_pos = scenePos();
-    if(scene_pos.x() > 200 || scene_pos.x() < -200 
-            || scene_pos.y() < -200 || scene_pos.y() > 200)
+    if(scene_pos.x() > 200 - CIRCLE_RADIUS || scene_pos.x() < -200 + CIRCLE_RADIUS
+            || scene_pos.y() < -200 + CIRCLE_RADIUS || scene_pos.y() > 200 - CIRCLE_RADIUS)
     {
         angle -= 90;
         setRotation(angle);
     }
-    setPos(mapToParent(0, 2));
+    setPos(mapToParent(0, 2 * SPEED));
 }
